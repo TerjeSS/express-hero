6a3ff5641d35a09637c1221bfd89f2c35f5de3ad
TerjeSlaatto <Terjeslaatto@gmail.com>
2022-02-11 21:11:58 +0100

    fjadsfjpsd

--- client/index.jsx
+++ client/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import {render} from "react-dom";
 import {
   BrowserRouter,
   HashRouter,
@@ -8,46 +8,23 @@ import {
   Routes,
 } from "react-router-dom";
 
-function FrontPage() {
-  return (
-    <div>
-      <h1>Welcome</h1>
-      <ul>
-        <li>
-          <Link to={"/login"}>Login</Link>
-        </li>
-      </ul>
-    </div>
-  );
+
+const Homepage = () => {
+    return(
+        <div>Welcome home</div>
+        )
+    }
+    
+    const App = () => {
+        
+        return (
+            <BrowserRouter>
+       <Routes>
+           <Route path={"/"} element={<Homepage />} />
+           <Route path={"/*"} element={<h2>Not found</h2>} />
+       </Routes>
+       </BrowserRouter>
+    )
 }
 
-function Login() {
-  return (
-    <form>
-      <h1>Login</h1>
-      <div>
-        Username <input />
-      </div>
-      <div>
-        Password <input type={"password"} />
-      </div>
-      <div>
-        <button>Login</button>
-      </div>
-    </form>
-  );
-}
-
-function Application() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={"/"} element={<FrontPage />} />
-        <Route path={"/login"} element={<Login />} />
-        <Route path={"/*"} element={<h1>Not Found</h1>} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
-
-ReactDOM.render(<Application />, document.getElementById("app"));
+render(<App />, document.getElementById("app"))
