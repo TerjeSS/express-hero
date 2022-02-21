39fc518aa400f66e3d9e606943e3170608c937f5
TerjeSlaatto <Terjeslaatto@gmail.com>
2022-02-21 18:47:56 +0100

    heroku cookie variable

--- client/index.jsx
+++ client/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import {
   BrowserRouter,
@@ -10,14 +10,79 @@ import {
 import Signin from "./Signin";
 
 const Homepage = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [user, setUser] = useState();
+  useEffect(async () => {
+    const res = await fetch("/api/login");
+    setUser(await res.json());
+    console.log(user);
+    if (user) {
+      setLoggedIn(!loggedIn);
+    }
+  }, []);
+  if (loggedIn) {
+    return <QuizApp user={user} />;
+  }
   return <Link to={"/signin"}>Log in</Link>;
 };
+
+function QuizApp({ user }) {
+  const [randomQuestion, setRandomQuestion] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+  const [score, setScore] = useState(0);
+
+  useEffect(async () => {
+    const res = await fetch("/api/quiz/random");
+    setRandomQuestion(await res.json());
+    setIsLoading(!isLoading);
+  }, []);
+
+  const handleAnswer = async (a) => {
+    const { id } = randomQuestion;
+    const res = await fetch("/api/quiz/answer", {
+      method: "post",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        id: id,
+        a,
+      }),
+    });
+    if (res.ok) {
+      setScore(score + 1);
+    }
+  };
+  return (
+    <>
+      <h1>Welcome to the quiz.</h1>
+      <h2>Your score is: {score} correct answers </h2>
+      {isLoading ? (
+        <div>
+          <h3>{randomQuestion.question}</h3>
+          {Object.keys(randomQuestion.answers)
+            .filter((a) => randomQuestion.answers[a])
+            .map((a) => {
+              return (
+                <div key={a}>
+                  {randomQuestion.answers[a]}
+                  <button onClick={() => handleAnswer(a)}>Answer</button>
+                </div>
+              );
+            })}
+        </div>
+      ) : (
+        <h2>Loading...</h2>
+      )}
+    </>
+  );
+}
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={"/"} element={<Homepage />} />
+        <Route path={"/"} element={<QuizApp />} />
         <Route path={"/signin"} element={<Signin />} />
         <Route path={"/*"} element={<h2>Not found</h2>} />
       </Routes>
--- server/server.js
+++ server/server.js
@@ -1,14 +1,15 @@
-import express from "express";
+import express, { response } from "express";
 import path from "path";
-import bodyparser from "express";
+import bodyparser from "body-parser";
 import cookieparser from "cookie-parser";
 import { urlencoded } from "express";
 import dotenv from "dotenv";
+import { isCorrectAnswer, Questions, randomQuestion } from "./questions.js";
 
 dotenv.config();
 
 const app = express();
-app.use(bodyparser());
+app.use(bodyparser.json());
 app.use(cookieparser(process.env.COOKIE_SECRET));
 app.use(
   urlencoded({
@@ -26,6 +27,22 @@ const users = [
     password: "terje",
   },
 ];
+
+//Destruct  a question to only return the question and answers, but without the "correct_answers".
+app.get("/api/quiz/random", (req, res) => {
+  const { question, answers, id } = randomQuestion();
+  res.json({ question, answers, id });
+});
+app.post("/api/quiz/answer", (req, res) => {
+  const { id, a } = req.body;
+  const question = Questions.find((q) => q.id === id);
+  if (isCorrectAnswer(question, a)) {
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
 //Custom middleware som kjører på hver eneste request.
 app.use((req, res, next) => {
   const { username } = req.signedCookies;
@@ -33,35 +50,30 @@ app.use((req, res, next) => {
   next();
 });
 
+app.get("/api/login", (req, res) => {
+  const { username } = req.signedCookies;
+  if (username) {
+    res.json(username);
+  } else {
+    res.sendStatus(401);
+  }
+});
+
+//Setter cookie ved suksesfull login, sletter cookie
 app.post("/login", (req, res, next) => {
   const { username, password } = req.body;
 
   const user = users.find((user) => user.username === username);
   if (user && user.password === password) {
     res.cookie("username", username, { signed: true });
-    res.redirect("/users");
+    res.redirect("/");
   } else {
     res.clearCookie("username");
     res.sendStatus(401);
   }
 });
 
-app.get("/login", (req, res) => {
-  if (!req.user) {
-    res.sendStatus(401);
-  } else {
-    res.send("logged in");
-  }
-});
-
-app.get("/users", (req, res) => {
-  if (!req.user) {
-    res.sendStatus(401);
-  } else {
-    res.send(users.map(({ username }) => ({ username })));
-  }
-});
-
+//Bruke statiske filer
 app.use(express.static("../client/dist"));
 
 app.use((req, res, next) => {
